424518da276a5bf7204471da85d225875face004
root <root@PC-HZZ.localdomain>
2021-07-23 12:00:07 +0800

    Issue:add updateMeta

--- proto/asset/asset.pb.micro.go
+++ proto/asset/asset.pb.micro.go
@@ -43,6 +43,7 @@ type AssetService interface {
 	UpdateSmall(ctx context.Context, in *ReqAssetFlag, opts ...client.CallOption) (*ReplyInfo, error)
 	GetToken(ctx context.Context, in *RequestInfo, opts ...client.CallOption) (*ReplyAssetToken, error)
 	UpdateBase(ctx context.Context, in *ReqAssetUpdate, opts ...client.CallOption) (*ReplyInfo, error)
+	UpdateMeta(ctx context.Context, in *ReqAssetFlag, opts ...client.CallOption) (*ReplyInfo, error)
 }
 
 type assetService struct {
@@ -147,6 +148,16 @@ func (c *assetService) UpdateBase(ctx context.Context, in *ReqAssetUpdate, opts 
 	return out, nil
 }
 
+func (c *assetService) UpdateMeta(ctx context.Context, in *ReqAssetFlag, opts ...client.CallOption) (*ReplyInfo, error) {
+	req := c.c.NewRequest(c.name, "AssetService.UpdateMeta", in)
+	out := new(ReplyInfo)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for AssetService service
 
 type AssetServiceHandler interface {
@@ -159,6 +170,7 @@ type AssetServiceHandler interface {
 	UpdateSmall(context.Context, *ReqAssetFlag, *ReplyInfo) error
 	GetToken(context.Context, *RequestInfo, *ReplyAssetToken) error
 	UpdateBase(context.Context, *ReqAssetUpdate, *ReplyInfo) error
+	UpdateMeta(context.Context, *ReqAssetFlag, *ReplyInfo) error
 }
 
 func RegisterAssetServiceHandler(s server.Server, hdlr AssetServiceHandler, opts ...server.HandlerOption) error {
@@ -172,6 +184,7 @@ func RegisterAssetServiceHandler(s server.Server, hdlr AssetServiceHandler, opts
 		UpdateSmall(ctx context.Context, in *ReqAssetFlag, out *ReplyInfo) error
 		GetToken(ctx context.Context, in *RequestInfo, out *ReplyAssetToken) error
 		UpdateBase(ctx context.Context, in *ReqAssetUpdate, out *ReplyInfo) error
+		UpdateMeta(ctx context.Context, in *ReqAssetFlag, out *ReplyInfo) error
 	}
 	type AssetService struct {
 		assetService
@@ -218,4 +231,8 @@ func (h *assetServiceHandler) GetToken(ctx context.Context, in *RequestInfo, out
 
 func (h *assetServiceHandler) UpdateBase(ctx context.Context, in *ReqAssetUpdate, out *ReplyInfo) error {
 	return h.AssetServiceHandler.UpdateBase(ctx, in, out)
+}
+
+func (h *assetServiceHandler) UpdateMeta(ctx context.Context, in *ReqAssetFlag, out *ReplyInfo) error {
+	return h.AssetServiceHandler.UpdateMeta(ctx, in, out)
 }
